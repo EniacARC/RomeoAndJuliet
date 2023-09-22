bbb7d6333e475c95031a36da312d0ea1a944bff6
U-LOZER <yonatan.chapal@gmail.com>
2023-09-22 16:01:10 +0300

    First Commit Setup Test Pycharm

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Author: Yonathan Chapal
+Program name: RomeoAndJuliet
+Description:
+Date: 22/9/2023
+"""
+
+
+def main():
+    print("Hello World")
+
+
+if __name__ == '__main__':
+    main()
